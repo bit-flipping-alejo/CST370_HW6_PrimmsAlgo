79fc5b6c87e7163d522b23efb20b44d753442ef3
bit-flipping-alejo <44530684+bit-flipping-alejo@users.noreply.github.com>
2020-02-25 15:16:59 -0800

    Update prim.cpp

--- prim.cpp
+++ prim.cpp
@@ -17,20 +17,24 @@ using namespace std;
 
 void displayEdges( int** array, int numEdges);
 void displayCostMatrix( int** array, int numVerts);
-
+void displayVisited(int* vis, int numVis);
 void populateNodeCost(int** edgeArray, int numEdges, int** nodeCostArray, int numNodes);
+void primsAlgo( int** cost, int numCosts, int* visited, int &numVisited);
+
 int main(){
 
 	string fileName;
 	ifstream infile;
 	int numNodes;
-
 	int numEdges;
-	int** edgeArray;
 
+	int** edgeArray;
 	int** nodeCostArray;
 
+	int startingNode;
 
+	int* visited;
+	int numVisited = 0;
 
 	cout << "Enter a file name: " ;
 	getline(cin, fileName);
@@ -71,6 +75,7 @@ int main(){
 		}
 	}
 
+	visited = new int[numNodes + 1]; // dont use 0th idx
 	nodeCostArray = new int*[numNodes + 1]; //were going to ignore row0 and col0
 											// so 4 nodes make a 5x5 so we can directly ref from input file
 
@@ -80,18 +85,32 @@ int main(){
 
 	populateNodeCost(edgeArray, numEdges, nodeCostArray, numNodes);
 
+	cout << "Enter a starting Vertex: ";
+	cin >> startingNode;
+
+	numVisited = 1;
+	visited[1] = startingNode;
+
+	primsAlgo( nodeCostArray, (numNodes + 1), visited, numVisited);
+
+	cout << "num visited: " << numVisited << endl;
+
 	displayCostMatrix(  nodeCostArray, numNodes+1);
-
 	//displayMatrix(edgeArray, numEdges);
+	displayVisited(visited, numVisited);
 
 
-
+	// Clean Up
 	for (int i = 0; i < numEdges; i++){
 		delete[] edgeArray[i];
 	}
+	for (int i = 0; i < (numNodes + 1); i++){
+		delete[] nodeCostArray[i];
+	}
 
 	delete[] edgeArray;
-
+	delete[] nodeCostArray;
+	delete[] visited;
 	return 0;
 }
 
@@ -107,7 +126,6 @@ void displayEdges( int** array, int numEdges){
 
 }
 
-
 void displayCostMatrix( int** array, int numVerts){
 	for (int i = 0; i < numVerts; i++){
 		for (int j = 0; j < numVerts; j++){
@@ -117,6 +135,14 @@ void displayCostMatrix( int** array, int numVerts){
 	}
 }
 
+void displayVisited(int* vis, int numVis){
+	cout << "Order Visited: ";
+	for (int i = 1; i <= numVis; i++){
+		cout << vis[i] << " ";
+	}
+	cout << endl;
+}
+
 void populateNodeCost(int** edgeArray, int numEdges, int** nodeCostArray, int numNodes){
 
 	for (int i = 0; i < (numNodes + 1); i++){
@@ -124,7 +150,6 @@ void populateNodeCost(int** edgeArray, int numEdges, int** nodeCostArray, int nu
 			nodeCostArray[i][j] = INT_MAX;
 		}
 	}
-
 
 	int row;
 	int col;
@@ -136,6 +161,79 @@ void populateNodeCost(int** edgeArray, int numEdges, int** nodeCostArray, int nu
 		nodeCostArray[row][col] = cost;
 	}
 }
+
+void primsAlgo( int** cost, int numCosts, int* visited, int &numVisited){
+
+	//start at starting node, be greedy
+	int minCost = INT_MAX;
+	int minIdx;
+	bool hasBeenVisited;
+	cout << "num visited = " << numVisited << endl;
+	cout << "numCosts = "  << numCosts << endl;
+	while (numVisited != numCosts){
+
+		minCost = INT_MAX;
+		for(int accessibleNodes = 1; accessibleNodes <= numVisited; accessibleNodes++ ){
+
+
+			for (int curCost = 1; curCost < numCosts; curCost++){
+				cout << "[" << visited[accessibleNodes] << "][" << curCost << "]" << endl;
+				// check visited nodes
+				for (int visNodes = 1; visNodes <= numVisited; visNodes++){
+					//cout << "visNode: " << visNodes << " || curCost: " << curCost << endl;
+					if (curCost == visited[visNodes] ){
+						//skip this
+						hasBeenVisited = true;
+						break;
+					}
+				}
+
+				if (hasBeenVisited){
+					//cout << "broke out of loop num: " << curCost << endl;
+					hasBeenVisited = false;
+					continue;
+				}
+
+				// Will reach here if it has NOT been visited
+				if (cost[ visited[accessibleNodes] ][curCost] < minCost){
+					// will reach here if currCost less than curMin
+					minCost = cost[ visited[accessibleNodes] ][curCost] ;
+					minIdx = curCost;
+				}
+
+			} // end curCost
+
+		} // end Accessible Nodes
+
+
+		if (minCost != INT_MAX){
+			numVisited++;
+			visited[numVisited] = minIdx;
+			cout << "minIdx:" << minIdx << " || cost: " << minCost;
+		} else {
+			// no more mins, so all done
+			cout << "oops" << endl;
+			break;
+		}
+
+	} // end while
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
